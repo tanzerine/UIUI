a4e676b574d0732377677530c076e235ec8c2ae2
tanzerine <73976340+tanzerine@users.noreply.github.com>
2024-06-17 02:37:26 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
@@ -11,15 +12,15 @@ const port = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 io.on('connection', (socket) => {
-  console.log('New client connected');
-
-  socket.on('newWord', (data) => {
-    io.emit('newWord', data);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('Client disconnected');
-  });
+    console.log('a user connected');
+    socket.on('newWord', (data) => {
+        io.emit('newWord', data);
+    });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 });
 
-server.listen(port, () => console.log(`Listening on port ${port}`));
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
